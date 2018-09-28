92076741f1acccc02508a69e6a3e22718f64af9b
qingduyu <flc009@163.com>
2018-09-28 13:41:13 +0800

    DRF 外键添加数据采用了 读写分开的DRF方式，很不错奥

---
+++ apps/CMDB/templatetags/mytags.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='int2str')
+def int2str(value):
+    """
+    int 转换为 str
+    """
+    return str(value)
+
+
+@register.filter(name='res_splict')
+def res_split(value):
+    """
+    将结果格式化换行
+    """
+    res = []
+    if isinstance(value, tuple):
+        for v in value:
+            if v is not None:
+                data = v.replace('\n', '<br>')
+                res.append(data)
+        return res
+    else:
+        return value
+
